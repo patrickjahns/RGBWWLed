bf24296209eda04f15766a4258d7c3055475b8be
Patrick Jahns <github@patrickjahns.de>
2016-03-08 10:25:37 +0100

    basis for documentation

--- RGBWWLedOutput.cpp
+++ RGBWWLedOutput.cpp
@@ -1,8 +1,23 @@
-/*
- * RGBWWLedOutput.cpp
+/**
+ * @file
+ * @author  Patrick Jahns http://github.com/patrickjahns
  *
- *  Created on: 21.02.2016
- *      Author: PJ
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * https://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ *
  */
 #include "RGBWWLed.h"
 #include "RGBWWLedOutput.h"
--- debugUtils.h
+++ debugUtils.h
@@ -1,7 +1,24 @@
-/*
-debugUtils.h - Simple debugging utilities.
-*/
-
+/**
+ * @file
+ * @author  Patrick Jahns http://github.com/patrickjahns
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * https://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ *
+ */
 #ifndef DEBUGUTILS_H
 #define DEBUGUTILS_H
 
